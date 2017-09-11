25fac4f509c918c48240f6c69f7acaa7859285ef
ottermegazord <idaly.ali@live.fr>
2017-09-10 20:03:10 -0400

    update guassian.py with generation of 1000 points, sample mean and covariance, and eigenvectors and eigenvalues, and indicate them on the graph

--- hw0/guassian.py
+++ hw0/guassian.py
@@ -26,30 +26,38 @@ import matplotlib.pyplot as plt
 
 mean = [4,2]
 cov = [[1,1.5] , [1.5,3]]
+n = 1000
 
 # Generate Gaussian points
-R = np.random.multivariate_normal(mean, cov, 1000)
+R = np.random.multivariate_normal(mean, cov, n)
 plt.scatter(R[:,0], R[:,1], marker='.')
 #plt.show()
 
 #Estimate sample mean of Gaussian distribution
-mu_sample = np.mean(R, axis=0) # axis -> axis or axes along which the means are computed
+mu_hat = np.mean(R, axis=0) # axis -> axis or axes along which the means are computed
 print("Sample mean is: ")
-print(mu_sample)
+print(mu_hat)
 print('\n')
 
-#Centering R at sample mean
-mu_sample_newaxis = mu_sample[np.newaxis, :]  # each row now contains 2 x 1 matrix, as opposed to one dimensional array
+# Centering R at sample mean
+mu_sample_newaxis = mu_hat[np.newaxis, :]  # each row now contains 2 x 1 matrix, as opposed to one dimensional array
                                               # length 3
-
+# Normalization against mean
 R_0 = R - mu_sample_newaxis
-cov_hat = np.dot(R_0.T, R_0)
+cov_hat = np.dot(R_0.T, R_0) # formula of covariance R.T dot R
 #cov_hat = np.cov(R_0)
 
 #Generate eigenvectors of the covariance matrix
 
-w, v = np.linalg.eig(cov_hat)
+w, v = np.linalg.eig(cov_hat) # w => ei
+print('Eigenvalue: ')
+print w
 print('Eigenvectors: ')
-print w, v
-#plt.arrow(mu_sample[0], mu_sample[1], v[0,0], v[1,0], shape='full', lw=4, head_width=.05)
+print v
+
+# Show eigenvectors on graph
+plt.arrow(mu_hat[0], mu_hat[1], v[0,0], v[1,0], shape='full', lw=4, head_width=.05)
+plt.arrow(mu_hat[0], mu_hat[1], v[1,0], v[1,1], shape='full', lw=4, head_width=.05)
+plt.scatter(R[:,0], R[:,1], marker='.')
 plt.show()
+
